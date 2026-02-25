a85981a8a0bdd09da6d1cf512638b5632766f6fc
mohd Affan <mohdaffanboy786@gmail.com>
2026-02-25 15:13:24 +0530

    types use like number,string,bool,null,undefined

---
+++ CLASS/L2P2.js
@@ -0,0 +1,16 @@
+var num = 10;
+console.log(num);
+console.log(typeof(num));
+num = false;
+console.log(num);
+console.log(typeof(num));
+num = null;
+console.log(num);
+console.log(typeof(num));
+num = undefined;
+console.log(num);
+console.log(typeof(num));
+num = "affan";
+console.log(num);
+console.log(typeof(num));
+
