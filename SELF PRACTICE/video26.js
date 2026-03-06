5eb33480f1840aec47139b362e38fd2b6e616172
mohd Affan <mohdaffanboy786@gmail.com>
2026-03-06 11:02:32 +0530

    learn about object and types like string null number undefined

---
+++ SELF PRACTICE/video26.js
@@ -0,0 +1,46 @@
+console.log("Hey this is tutorial 55");
+//jha let h bha var bhi likh sakte 
+//var globally scoped h or let block scoped h 
+//let ma bad m bracet(braket ko js m block khenge ki block m likha h)
+//  h ander value dono show krega pr
+// var h last wali value show krega dono bar 
+var a = 5;
+// a = a + 1
+let b = 6;
+let c = "Harry";
+let _a = "Shubham";
+// var 55a = "Rohan"; // Not Allowed
+
+// console.log(a +  b + 8);
+// console.log(typeof a, typeof b, typeof c);
+{
+    // let a = 66;
+    console.log(a)//yo
+}
+console.log(a)//or yo dono likhega to 2 value milegi a ki 
+// const a1 = 6;
+// a1 = a1 + 1; // Not Allowed because a1 is constant
+
+
+let x = "Harry bhai";
+let y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+
+let o = {
+    "name": "Harry",
+    "job code": 5600,
+    "is_handsome": true
+}
+
+console.log(o);
+o.salary = "100crores";
+console.log(o);
+o.salary = "500crores";
+console.log(o);
